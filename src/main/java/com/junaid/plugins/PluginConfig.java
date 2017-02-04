f7f3bbe5a411e747f032b245159dd01be28a17c1
junaidAnwar <junaid2905@gmail.com>
2017-02-04 12:31:04 +0530

    commmenting pluginConfig.java file

--- src/main/java/com/junaid/plugins/PluginConfig.java
+++ src/main/java/com/junaid/plugins/PluginConfig.java
@@ -1,10 +1,10 @@
-package com.junaid.plugins;
-
-import com.eviware.soapui.plugins.PluginAdapter;
-import com.eviware.soapui.plugins.PluginConfiguration;
-
-@PluginConfiguration(groupId = "com.junaid.plugins.plugins", name = "regressor-plugin SoapUI TestStep", version = "0.1",
-        autoDetect = true, description = "regressor-plugin SoapUI TestStep",
-        infoUrl = "" )
-public class PluginConfig extends PluginAdapter {
-}
+//package com.junaid.plugins;
+//
+//import com.eviware.soapui.plugins.PluginAdapter;
+//import com.eviware.soapui.plugins.PluginConfiguration;
+//
+//@PluginConfiguration(groupId = "com.junaid.plugins.plugins", name = "regressor-plugin SoapUI TestStep", version = "0.1",
+//        autoDetect = true, description = "regressor-plugin SoapUI TestStep",
+//        infoUrl = "" )
+//public class PluginConfig extends PluginAdapter {
+//}
